44ac867605b8f2a7159959189168f7e2bc51ae7e
RohanSeshasaiVasa <90746722+RohanSeshasaiVasa@users.noreply.github.com>
2025-06-30 16:41:14 +0000

    Show product link in alert box on Buy page

---
+++ Buy.js
@@ -0,0 +1,12 @@
+function showProductLink() {
+    alert("Product link: https://robu.in/product/raspberry-pi-5-model-8gb/")
+}
+function showProductLink() {
+    alert("Product link: https://robu.in/product/raspberry-pi-3-model-a-with-official-raspberry-pi-case/")
+}
+function showProductLink() {
+    alert("Product link: https://robu.in/product/raspberry-pi-5-model-2gb/")
+}
+function showProductLink() {
+    alert("Product link: Get tricked (There is no derivation of this product)")
+}
