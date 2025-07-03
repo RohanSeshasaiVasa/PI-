adb49c0110e3c15143d982e4aac825f24073e095
RohanSeshasaiVasa <90746722+RohanSeshasaiVasa@users.noreply.github.com>
2025-07-03 12:49:31 +0000

    only one function did

--- Buy.js
+++ Buy.js
@@ -1,12 +1,12 @@
-function showProductLink() {
-    alert("Product link: https://robu.in/product/raspberry-pi-5-model-8gb/")
+function showProductLink(link) {
+    alert("Product link: " + link);
 }
-function showProductLink() {
-    alert("Product link: https://robu.in/product/raspberry-pi-3-model-a-with-official-raspberry-pi-case/")
+function showProductLink(link) {
+    alert("Product link: " + link);
 }
-function showProductLink() {
-    alert("Product link: https://robu.in/product/raspberry-pi-5-model-2gb/")
+function showProductLink(link) {
+    alert("Product link: " + link);
 }
-function showProductLink() {
-    alert("Product link: Get tricked (There is no derivation of this product)")
+function showProductLink(link) {
+    alert("Product link: " + link);
 }
